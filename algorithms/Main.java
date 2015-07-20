7e3d8eb4608a0e8431678ad50cbc7e124cf5b4dd
nandkishore <nandkishore.sharma@rtslabs.com>
2015-07-20 19:51:57 -0400

    Main file improved

--- algorithms/Main.java
+++ algorithms/Main.java
@@ -6,21 +6,33 @@ import Graph.utils.CreateGraph;
 import java.util.Iterator;
 
 /**
+ * Demo of how to run those algorithms
  * Created by Nandkishore on 7/15/2015.
  */
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    private void runTopologicalSort() {
         Graph g = new CreateGraph(true).getGraph();
-        //g.print();
-       // new BFS().traverse(g,g.getVertex("A"));
-        /*System.out.println("Prims Edges set : ");
+        print(new TopologicalSort(g).order);
+    }
+
+    private void runDijsktras() {
+        Graph g = new CreateGraph(false).getGraph();
+        new Dijsktras(g).shortestPath(g.getVertex("A"), g.getVertex("B"));
+    }
+    private void runPrims() {
+        Graph g = new CreateGraph(false).getGraph();
         Iterator it =  new PrimsMST(g).mstEdgeSet.iterator();
         while(it.hasNext()){
             System.out.println(it.next());
-        }*/
-        //new Dijsktras(g).shortestPath(g.getVertex("A"), g.getVertex("B"));
-        print(new TopologicalSort(g).order);
-
+        }
+    }
+    private void printGraph() {
+        Graph g = new CreateGraph(false).getGraph();
+        g.print();
     }
 
     private static void print(Object o) {
